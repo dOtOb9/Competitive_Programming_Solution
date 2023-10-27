22819b75f2e5e13b0149d33d7cc4ca25a9da2da2
kuku <124516137+CVm1219@users.noreply.github.com>
2023-10-28 01:13:48 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
