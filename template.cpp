dd341550996b8fedfc7cd86241ddb92cec9db2f6
dOtOb9 <dotob9@DESKTOP-SB6EEAL.>
2024-11-21 00:59:18 +0900

    久しぶりに更新

--- template.cpp
+++ template.cpp
@@ -16,6 +16,8 @@ using ull = unsigned long long;
 const int inf =  2147483647;         // 2e9     1 << 30
 const ll INF = 9223372036854775807;  // 9e18    1LL << 60
 
-#define rep(i, n) for (int i = 0; i < n; ++i)
+#define r(i, n) for (int i = 0; i < n; ++i)
+#define r1(i, n) for (int i = 1; i < n; ++i)
+#define r0(i) for (int i = -1; i < 2; ++i)
 #define pii pair<int, int>
 
