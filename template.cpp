1601bb9890cfc968138e1bb23636ab1969c2b5f3
dOtOb9 <dotob9@gmail.com>
2025-02-28 12:27:44 +0900

    template is added UnionFind and YesNo

--- template.cpp
+++ template.cpp
@@ -21,6 +21,46 @@ const l INF = 9223372036854775807;  // 9e18    1LL << 60
 #define r0(i) for (l i = -1; i < 2; ++i)
 #define pll pair<l, l>
 
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(l x, l y) {
+        l rx = find(x);
+        l ry = find(y);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(l x, l y) {
+        return find(x) == find(y);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
 int main() {
     
 }
