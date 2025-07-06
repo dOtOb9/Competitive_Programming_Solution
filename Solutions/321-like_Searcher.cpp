ec6636aabc7e7627c65349acb4634297993df8f7
dOtOb9 <dotob9@gmail.com>
2025-07-06 10:11:16 +0900

    add 321-like Searcher

---
+++ Solutions/321-like_Searcher.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+void nans(l ans) {
+    if (ans==INF) cout<<-1<<endl;
+    else cout<<ans<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(pll xy) {
+        l rx = find(xy.first);
+        l ry = find(xy.second);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(pll xy) {
+        return find(xy.first) == find(xy.second);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+
+int main() {
+    l K;cin>>K;
+
+    vector<l> a;
+
+    r(i, 1 << 10) {
+        l x = 0;
+
+        r(j, 10) {
+            if (i & 1 << j) {
+                x = x * 10 +  9-j;
+            }
+        }
+
+        if (x==0) continue;
+
+        a.push_back(x);
+    }
+
+    sort(a.begin(), a.end());
+
+    cout << a[K-1] << endl;
+}
