253d6cea9221ca2042afc24ff9b2b7e1ba33587e
dOtOb9 <dotob9@gmail.com>
2025-07-06 19:38:10 +0900

    add Count Simple Paths

---
+++ Solutions/Count_Simple_Paths.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+void nans(l ans) {
+    if (ans==INF) cout<<-1<<endl;
+    else cout<<ans<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(pll xy) {
+        l rx = find(xy.first);
+        l ry = find(xy.second);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(pll xy) {
+        return find(xy.first) == find(xy.second);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+
+int main() {
+    l h, w, K;cin>>h>>w>>K;
+
+    vector<string> f(h);
+
+    r(i, h) cin>>f[i];
+
+    l ans = 0;
+
+    function<void(l,l,l, vector<string>)> dfs = [&](l y, l x, l cnt, vector<string> sp) {
+        sp[y][x] = '*';
+
+        if (cnt == K) {
+            ++ans;
+            return;
+        }
+
+        ++cnt;
+
+        r0(dy)r0(dx) {
+            if (dy!=0 && dx != 0) continue;
+            l ny = dy+y, nx = dx + x;
+
+            if (ny < 0 || ny >= h || nx < 0 || nx >= w) continue;
+
+            if (f[ny][nx]=='#') continue;
+            if (sp[ny][nx]=='*') continue;
+
+            dfs(ny, nx, cnt, sp);
+        }
+    };
+
+    r(i, h) r(j, w) {
+        if (f[i][j]=='#') continue;
+
+        auto fi = f;
+
+        dfs(i, j, 0, fi);
+    }
+
+    cout << ans << endl;
+}
