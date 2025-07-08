7bcb463f71f6ed114a9b6f76e68e43fb0668af65
dOtOb9 <dotob9@gmail.com>
2025-07-08 22:17:11 +0900

    add X drawing

---
+++ Solutions/X_drawing.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(l x, l y) {
+        l rx = find(x);
+        l ry = find(y);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(l x, l y) {
+        return find(x) == find(y);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+int main() {
+    l n, a, b;cin >> n >> a >> b;
+
+    l P, Q, R, S;cin >> P>>Q>>R>>S;
+
+    for (l i = P; i < Q+1; ++i) for (l j = R; j < S+1; ++j) {
+        l k = i - a;
+
+        if (b+k == j) {
+            if (max(1-a, 1-b) <= k && k <= min(n-a, n-b)) cout << "#";
+            else cout << ".";
+        } else if (b-k == j) {
+            if (max(1-a, b-n) <= k && k <= min(n-a, b-1)) cout << "#";
+            else cout << ".";
+        } else {
+            cout << ".";
+        }
+
+        if (j == S) cout << endl;
+    }
+}
