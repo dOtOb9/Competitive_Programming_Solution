824d3ddb7825dd125a4b6bceccf95b5d0e311532
dOtOb9 <dotob9@gmail.com>
2025-07-12 02:58:27 +0900

    add Remembering the Days

---
+++ Solutions/Remembering_the_Days.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+#include <numeric>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(pll xy) {
+        l rx = find(xy.first);
+        l ry = find(xy.second);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(pll xy) {
+        return find(xy.first) == find(xy.second);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+int main() {
+    l n, m;cin >> n>> m;
+
+    vector<vector<pll>> g(n);
+
+    r(i, m) {
+        l a, b, c;cin >> a >> b >> c;--a;--b;
+
+        g[a].push_back({c, b});
+        g[b].push_back({c, a});
+    }
+
+    l ans = 0;
+
+    r(i, n) {
+
+        function<void(l,l, vector<bool>)> dfs=[&](l cost, l u, vector<bool> vs) {
+            vs[u] = true;
+
+            ans = max(ans, cost);
+
+            for (auto [c, v] : g[u]) {
+                if (vs[v]) continue;
+
+                dfs(cost + c, v, vs);
+            }
+        };
+
+        dfs(0, i, vector<bool>(n));
+    }
+
+    cout << ans << endl;
+}
