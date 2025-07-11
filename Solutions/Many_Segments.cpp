63a5b17b96fa4ca20638732f0f8e6d4bf92cbc94
dOtOb9 <dotob9@gmail.com>
2025-07-11 16:46:56 +0900

    add Many Segments

---
+++ Solutions/Many_Segments.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(l x, l y) {
+        l rx = find(x);
+        l ry = find(y);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(l x, l y) {
+        return find(x) == find(y);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+int main() {
+    l n;cin >> n;
+
+    vector<double> li(n), ri(n);
+
+    r(i, n) {
+        l t;cin >> t >> li[i] >> ri[i];
+
+        if (t == 2 || t == 4) ri[i] -= 0.5;
+        if (t == 3 || t == 4) li[i] += 0.5;
+    }
+
+    l ans = 0;
+
+    r(i, n) r(j, i) ans += (max(li[i], li[j]) <= min(ri[i], ri[j])); 
+
+    cout << ans << endl;
+}
