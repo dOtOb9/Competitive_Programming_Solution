8dc1f910dcbb5eb1c906e489b8eb3a0ef6ff703e
dOtOb9 <dotob9@gmail.com>
2025-07-14 20:14:19 +0900

    add Operations on a Stack

---
+++ Operations_on_a_Stack.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <functional>
+#include <stack>
+#include <numeric>
+
+using namespace std;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+struct UnionFind {
+    vector<l> parent, rank, size;
+    l tree_count;
+
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
+    }
+    bool unite(pll xy) {
+        l rx = find(xy.first);
+        l ry = find(xy.second);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
+        parent[ry] = rx;
+        size[rx] += size[ry];
+        if (rank[rx] == rank[ry]) rank[rx]++;
+
+        tree_count--;
+
+        return true;
+    }
+    
+    bool same(pll xy) {
+        return find(xy.first) == find(xy.second);
+    }
+    
+    l getSize(l x) {
+        return size[find(x)];
+    }
+};
+
+int main() {
+    l n;cin >> n;
+    vector<l> a(n);
+    r(i, n) cin >> a[i];
+
+    vector<pll> dp(n+1);
+
+    r1(i, n+1) {
+        dp[i] = {dp[i-1].first + a[i-1], dp[i-1].second+1};
+
+        if (i >= 2) if (dp[i-2].second >= 0 && dp[i-2].first > dp[i].first) {
+            dp[i] = {dp[i-2].first, dp[i-2].second};
+        } 
+    }
+
+    cout << dp[n].first << endl;
+}
