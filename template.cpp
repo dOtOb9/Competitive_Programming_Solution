75c8d05b0e4d96dca19a1d44e6896efa4aeb24fe
dOtOb9 <dotob9@gmail.com>
2025-08-29 16:14:54 +0900

    modify template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,8 @@
 #include <atcoder/all>
 
 using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
 using l = long long;
 using ul = unsigned long long;
 
@@ -20,52 +22,6 @@ void YesNo(bool s = false)
     else
         cout << "No" << endl;
 }
-
-struct UnionFind
-{
-    vector<l> parent, rank, size;
-    l tree_count;
-
-    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n)
-    {
-        for (l i = 0; i < n; i++)
-            parent[i] = i;
-    }
-
-    l find(l x)
-    {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]); // パス圧縮
-    }
-    bool unite(pll xy)
-    {
-        l rx = find(xy.first);
-        l ry = find(xy.second);
-        if (rx == ry)
-            return false;
-        if (rank[rx] < rank[ry])
-            swap(rx, ry);
-        parent[ry] = rx;
-        size[rx] += size[ry];
-        if (rank[rx] == rank[ry])
-            rank[rx]++;
-
-        tree_count--;
-
-        return true;
-    }
-
-    bool same(pll xy)
-    {
-        return find(xy.first) == find(xy.second);
-    }
-
-    l getSize(l x)
-    {
-        return size[find(x)];
-    }
-};
 
 //Era(n)を呼んだ後、isprime[i]=iが素数かどうか　となっている。
 vector < bool > isprime;
