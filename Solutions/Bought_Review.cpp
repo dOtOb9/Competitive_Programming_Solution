e594ace4dd8d201e469c57c4356d54038b4fa58e
dOtOb9 <dotob9@gmail.com>
2025-08-29 20:04:39 +0900

    add Bought Review

---
+++ Solutions/Bought_Review.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf = 2147483647;        // 2e9     1 << 30
+const l INF = 9223372036854775807; // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s = false)
+{
+    if (s)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+//Era(n)を呼んだ後、isprime[i]=iが素数かどうか　となっている。
+vector < bool > isprime;
+//返り値は素数のリスト。
+vector < l > Era(int n) {
+	isprime.resize(n, true);
+	vector < l > res;
+	isprime[0] = false;
+	isprime[1] = false;
+	for(l i = 2; i < n; ++i) isprime[i] = true;
+	for(l i = 2; i < n; ++i) {
+		if(isprime[i]) {
+			res.push_back(i);
+			for(l j = i * 2; j < n; j += i) isprime[j] = false;
+		}
+	}
+	return res;
+}
+
+//==================================================================================
+
+int main() {
+    l t;cin >> t;
+
+    r(_, t) {
+        vector<l> a(5), p(5);
+        r(i, 5) cin >> a[i];
+        
+        r(i, 5) cin >> p[i];
+
+        l sum = 0;
+        r(i, 5) sum += a[i] *(i-2);
+
+        if (sum < 0) {
+            sum *= -1;
+        } else {
+            cout << 0 << endl;
+            continue;
+        }
+
+        if (sum % 2 == 0) {
+            cout << sum/2 * min(p[4], p[3] * 2) << endl;
+        } else {
+            cout << sum/2 * min(p[4], p[3] * 2) + min(p[4], p[3]) << endl;
+        }
+    }
+}
