21177f2596ee1ea906ff8c2a7c88744ea960c303
dOtOb9 <dotob9@gmail.com>
2025-08-31 16:41:53 +0900

    add Colorful Subsequence

---
+++ Solutions/Colorful_Subsequence.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf = 2147483647;        // 2e9     1 << 30
+const l INF = 9223372036854775807; // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s = false)
+{
+    if (s)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+//Era(n)を呼んだ後、isprime[i]=iが素数かどうか　となっている。
+vector < bool > isprime;
+//返り値は素数のリスト。
+vector < l > Era(int n) {
+	isprime.resize(n, true);
+	vector < l > res;
+	isprime[0] = false;
+	isprime[1] = false;
+	for(l i = 2; i < n; ++i) isprime[i] = true;
+	for(l i = 2; i < n; ++i) {
+		if(isprime[i]) {
+			res.push_back(i);
+			for(l j = i * 2; j < n; j += i) isprime[j] = false;
+		}
+	}
+	return res;
+}
+
+//==================================================================================
+
+int main() {
+    l n;cin >> n;
+    string s;cin >> s;
+
+    vector<l> cnt(26);
+
+    for (char c : s) {
+        cnt[c-'a']++;
+    }
+
+    l ans = 1;
+
+    r(i, 26) {
+        ans *= cnt[i]+1;
+        ans %= (l)1e9+7;
+    }
+
+    cout << ans-1 << endl;
+}
