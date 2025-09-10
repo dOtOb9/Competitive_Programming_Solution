fa611b04367f6e1e00c89825fe6f0110afc02f39
dOtOb9 <dotob9@gmail.com>
2025-09-11 04:16:23 +0900

    add AtCoder AAC Contest

---
+++ Solutions/AtCoder_AAC_Contest.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
+using l = long long;
+using ul = unsigned long long;
+
+const int inf = 2147483647;        // 2e9     1 << 30
+const l INF = 9223372036854775807; // 9e18    1LL << 60
+
+#define r(i, n) for (l i = 0; i < n; ++i)
+#define r1(i, n) for (l i = 1; i < n; ++i)
+#define r0(i) for (l i = -1; i < 2; ++i)
+#define pll pair<l, l>
+
+void YesNo(bool s = false)
+{
+    if (s)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+//Era(n)を呼んだ後、isprime[i]=iが素数かどうか　となっている。
+vector < bool > isprime;
+//返り値は素数のリスト。
+vector < l > Era(int n) {
+	isprime.resize(n, true);
+	vector < l > res;
+	isprime[0] = false;
+	isprime[1] = false;
+	for(l i = 2; i < n; ++i) isprime[i] = true;
+	for(l i = 2; i < n; ++i) {
+		if(isprime[i]) {
+			res.push_back(i);
+			for(l j = i * 2; j < n; j += i) isprime[j] = false;
+		}
+	}
+	return res;
+}
+
+//==================================================================================
+
+int main() {
+    l t;cin >> t;
+    r(_, t) {
+        l na, nb, nc;cin >> na >> nb >> nc;
+
+        cout << min(min(na, nc), (na+nb+nc)/3) << endl;
+    }
+}
