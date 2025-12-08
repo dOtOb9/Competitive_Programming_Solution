c7e89fe589a7bbec10d59ad00db35ae1012c912e
dOtOb9 <dotob9@vivaldi.net>
2025-12-09 07:11:28 +0900

    revise bitmask

--- Solutions/Bitmask.cpp
+++ Solutions/Bitmask.cpp
@@ -15,75 +15,63 @@ using namespace std;
 using l = long long;
 using ul = unsigned long long;
 
-const int inf = 2147483647;        // 2e9     1 << 30
-const l INF = 9223372036854775807; // 9e18    1LL << 60
+const int inf =  2147483647;         // 2e9     1 << 30
+const l INF = 9223372036854775807;  // 9e18    1LL << 60
 
 #define r(i, n) for (l i = 0; i < n; ++i)
 #define r1(i, n) for (l i = 1; i < n; ++i)
 #define r0(i) for (l i = -1; i < 2; ++i)
 #define pll pair<l, l>
 
-void YesNo(bool s = false)
-{
-    if (s)
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
+void YesNo(bool s=false) {
+    if (s) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
 }
 
-struct UnionFind
-{
+struct UnionFind {
     vector<l> parent, rank, size;
     l tree_count;
 
-    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n)
-    {
-        for (l i = 0; i < n; i++)
-            parent[i] = i;
+    UnionFind(l n) : parent(n), rank(n, 0), size(n, 1), tree_count(n) {
+        for (l i = 0; i < n; i++) parent[i] = i;
     }
-
-    l find(l x)
-    {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]); // パス圧縮
+    
+    l find(l x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // パス圧縮
     }
-    bool unite(pll xy)
-    {
-        l rx = find(xy.first);
-        l ry = find(xy.second);
-        if (rx == ry)
-            return false;
-        if (rank[rx] < rank[ry])
-            swap(rx, ry);
+    bool unite(l x, l y) {
+        l rx = find(x);
+        l ry = find(y);
+        if (rx == ry) return false;
+        if (rank[rx] < rank[ry]) swap(rx, ry);
         parent[ry] = rx;
         size[rx] += size[ry];
-        if (rank[rx] == rank[ry])
-            rank[rx]++;
+        if (rank[rx] == rank[ry]) rank[rx]++;
 
         tree_count--;
 
         return true;
     }
-
-    bool same(pll xy)
-    {
-        return find(xy.first) == find(xy.second);
+    
+    bool same(l x, l y) {
+        return find(x) == find(y);
     }
-
-    l getSize(l x)
-    {
+    
+    l getSize(l x) {
         return size[find(x)];
     }
 };
 
+
 int main() {
     string s;cin >> s;
     l n;cin >> n;
-    vector<l> p;
+
+    vector<l> ps;
     r(i, s.size()) {
         if (s[i] == '?') {
-            p.push_back(i);
+            ps.push_back(i);
         }
     }
 
@@ -92,11 +80,14 @@ int main() {
         if (s[i] == '1') res |= 1LL << (s.size()-1 - i);
     }
 
-    r(i, p.size()) {
-        if (n >= (res | (1LL << (s.size()-1-p[i])))) res = res | (1LL << (s.size()-1-p[i]));
+    for (l p : ps) {
+        l sample = res | (1LL << (s.size()-1-p));
+        if (n >= sample) res = sample;
     }
 
     if (res > n) {
         cout << -1 << endl;
-    } else cout << res << endl;
+    } else {
+        cout << res << endl;
+    }
 }
